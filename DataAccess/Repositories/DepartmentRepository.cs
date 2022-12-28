b6f3a6c160acc8e36e241e39044e9ed3af2039c1
kamranss <kamranss@code.edu.az>
2022-12-28 20:14:23 +0400

    Refactor: Dataaccess Methods Implemented

--- DataAccess/Repositories/DepartmentRepository.cs
+++ DataAccess/Repositories/DepartmentRepository.cs
@@ -1,12 +1,87 @@
-﻿using System;
+﻿using DataAccess.Interfaces;
+using Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Repositories
 {
-    internal class GroupRepository
+    public class DepartmentRepository : IRepository<Department>
     {
+        public bool Create(Department entity)
+        {
+            try
+            {
+                AppDbContext.Departments.Add(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Delete(Department entity)
+        {
+            try
+            {
+                AppDbContext.Departments.Remove(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Department Get(Predicate<Department> filter = null)
+        {
+            try
+            {
+                return filter != null ? AppDbContext.Departments.Find(filter) : AppDbContext.Departments[0];
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Department> GetAll(Predicate<Department> filter = null)
+        {
+            try
+            {
+                return filter != null ? AppDbContext.Departments.FindAll(filter) : AppDbContext.Departments;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Update(Department entity)
+        {
+            try
+            {
+                Department department = Get(dept => dept.Id == entity.Id);
+                if (department != null)
+                {
+                    department = entity;
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
