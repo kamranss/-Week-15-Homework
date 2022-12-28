8618134e201b2c43c72416d752c0a5095365676f
kamranss <kamranss@code.edu.az>
2022-12-28 20:51:31 +0400

    Refactor: Added Service Layer

--- DataAccess/Repositories/EmployeeRepository.cs
+++ DataAccess/Repositories/EmployeeRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccess.Interfaces;
+using Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,72 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Repositories
 {
-    public class EmployeeRepository
+    public class EmployeeRepository : IRepository<Employee>
     {
+        public bool Create(Employee entity)
+        {
+            try
+            {
+                AppDbContext.Employees.Add(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Delete(Employee entity)
+        {
+            AppDbContext.Employees.Remove(entity);
+            return true;
+        }
+
+        public Employee Get(Predicate<Employee> filter = null)
+        {
+            try
+            {
+                return filter != null ? AppDbContext.Employees.Find(filter) : AppDbContext.Employees[0];
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Employee> GetAll(Predicate<Employee> filter = null)
+        {
+            try
+            {
+                return filter != null ? AppDbContext.Employees.FindAll(filter) : AppDbContext.Employees;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Update(Employee entity)
+        {
+            try
+            {
+                Employee employee = Get(emp => emp.Id == entity.Id);
+                if (entity != null)
+                {
+                    employee = entity;
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
---
+++ Service/Service/DepartmentService.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Models;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service
+{
+    public class DepartmentService : IDepartmentService
+    {
+        public Department Create(Department departmenbt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department Delete(int Id, Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department GetALL(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department Update(Department department)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
