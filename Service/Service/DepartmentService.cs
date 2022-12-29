015c01dacb0a93bdabce05cc918d5479e810d091
kamranss <kamranss@code.edu.az>
2022-12-29 23:23:14 +0400

    Refactor: updated

--- Service/Service/DepartmentService.cs
+++ Service/Service/DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Models;
+﻿using DataAccess.Repositories;
+using Domain.Models;
 using Service.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,34 @@ namespace Service.Service
 {
     public class DepartmentService : IDepartmentService
     {
-        public Department Create(Department departmenbt)
+        private readonly DepartmentRepository departmentRepository;
+        public static int Id { get; set; }
+        public DepartmentService()
         {
-            throw new NotImplementedException();
+            departmentRepository = new DepartmentRepository();
+        }
+        public Department Create(Department department)
+        {
+            try
+            {
+                Department tempdepartment = departmentRepository.Get(d => d.Name == department.Name);
+                if (tempdepartment==null)
+                {
+                    department.Id = Id;
+                    if (departmentRepository.Create(department))
+                    {
+                        Id++;
+                        return department;
+                    }
+                    return null;
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public Department Delete(int Id, Department department)
@@ -30,9 +56,18 @@ namespace Service.Service
             throw new NotImplementedException();
         }
 
-        public Department GetALL(string name)
+        public List<Department> GetALL(string name)
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Department> tempdepartment = departmentRepository.GetAll(d => d.Name == name);
+                return tempdepartment;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
         }
 
         public Department Update(Department department)
