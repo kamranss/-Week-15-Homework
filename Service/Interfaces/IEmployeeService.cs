0453120f0d38b49d0c5cccb24da1bfba1b670c22
kamranss <kamranss@code.edu.az>
2022-12-30 23:42:56 +0400

    Refactor: Employee Service started

---
+++ Service/Interfaces/IEmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    public interface IStudentService
+    {
+        Employee Create(Employee department);
+        Employee Update(Employee department);
+        Employee Delete(int id, Employee department);
+        Employee Get(int id);
+        Employee Get(string name);
+        List<Employee> GetAll(string name);
+
+    }
+}
---
+++ Service/Service/EmployeeService.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Models;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service
+{
+    internal class EmployeeService : IEmployeeService
+    {
+        public Employee Create(Employee department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Delete(int id, Employee department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetAll(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Update(Employee department)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
